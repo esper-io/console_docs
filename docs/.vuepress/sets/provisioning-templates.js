1b0ce4f771ea7b995163d593895a5a7f478724e1
Radha Kiwalkar <radha@esper.io>
2021-03-01 21:25:08 -0800

    adding sub headings

--- docs/.vuepress/sets/provisioning-templates.js
+++ docs/.vuepress/sets/provisioning-templates.js
@@ -17,6 +17,9 @@ module.exports = {
         collapsable: false,
         children: [
           "",
+          "actions",
+          "createtemplate",
+          "upload-imei",
         ]
       },
     ]
