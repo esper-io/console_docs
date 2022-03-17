7529f7d722b038ddaf69727f0e6cd4eace3ce504
Radha Kiwalkar <radha@esper.io>
2022-03-17 10:48:32 -0700

    added a billing page

---
+++ docs/.vuepress/sets/billing.js
@@ -0,0 +1,43 @@
+module.exports = {
+    title: "Esper Console Documentation | %v",
+    setTitle: "Billing",
+    handle: "console",
+    icon: "/icons/sidebar/company_settings.svg",
+    baseDir: "company-settings",
+    // versions: [
+    //   ["introduction", { label: "1.x" }]
+    // ],
+    // defaultVersion: "introduction",
+    searchPlaceholder: "Search Company Settings (Press “/” to focus)",
+    primarySet: true,
+    sidebar: {
+      "/": [
+        {
+          title: "Introduction",
+          collapsable: false,
+          children: [
+            "",
+          ]
+        },
+      ]
+    }
+    // locales: {
+    //   "/": {
+    //     lang: "en-US",
+    //     name: "English",
+    //     title: "Esper Console Documentation | %v",
+    //     config: require("./Introduction/introduction-to-esper-en.js")
+    //   },
+      // "/ja/": {
+      //   lang: "ja",
+      //   title: "Esper Console ドキュメント | %v",
+      //   config: require("./esper-console-ja.js")
+      // },
+      // "/fr/": {
+      //   lang: "fr",
+      //   title: "Esper Console Documentation | %v",
+      //   config: require("./esper-console-fr.js")
+      // }
+    // }
+  };
+  
---
+++ docs/billing/README.md
@@ -0,0 +1,33 @@
+# What is the Billing Section?
+
+  
+
+Billing is a section in the Console to help users purchase the Ops plan, our basic MDM-catered offering.
+
+  
+:::tip
+Only Enterprise Admin users will be able to purchase this plan. Enterprise viewer user role will be able to view the plan. It will not be accessible to Viewer and the Group Viewer user roles.
+:::
+  
+
+On the Console, click the profile icon and choose the ‘Billing’option from the menu.
+
+![choose billing option](./images/1-billingOption.png)
+
+The landing page displays the current plan, the number of devices onboarded, and the start date.
+
+![billing landing page](./images/2-main.png)
+
+Click the **View Invoice** link to view past purchases.
+
+![invoice](./images/3-invoice.png)
+
+  
+
+Here, you can purchase the Ops plan. Click **Select**, and a typical payment modal is displayed. You can choose the monthly or the annual plan.
+
+![payment modal](./images/4-payment.png)
+
+  
+
+You can upgrade to DevOps and Enterprise plans anytime. [Esper Sales](https://www.esper.io/contact-us) team will be happy to help!
