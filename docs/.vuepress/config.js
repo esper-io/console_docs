3256508ce2d1c660c6e17307fb95bf062023655d
Radha Kiwalkar <radha@esper.io>
2022-03-28 13:44:06 -0700

    Added ESD page

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -74,7 +74,7 @@ module.exports = {
       require('./sets/company-settings'),
       require('./sets/user-management'),
       require('./sets/billing'),
-     // require('./sets/esper-enhanced'),
+      require('./sets/esper-enhanced'),
       require('./sets/api-key-management'),
       require('./sets/misc'),
     ],
