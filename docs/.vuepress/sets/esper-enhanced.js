4f23a23f49087487af6a434da095116c551fbdad
Radha Kiwalkar <radha@esper.io>
2022-04-04 09:45:28 -0700

    enhanced to software

--- docs/.vuepress/sets/esper-enhanced.js
+++ docs/.vuepress/sets/esper-enhanced.js
@@ -3,7 +3,7 @@ module.exports = {
   setTitle: "Esper Software Updates",
   handle: "console",
   icon: "/icons/sidebar/esper_enhanced.svg",
-  baseDir: "esper-enhanced-updates",
+  baseDir: "esper-software-updates",
   // versions: [
   //   ["introduction", { label: "1.x" }]
   // ],
